87054f5d344a685a087bdfe3724f91668bf1e9ce
kroshaResheniye <kroshaResheniye@gmail.com>
2023-03-09 03:56:50 -0300

    Codigos

---
+++ Codigos/Calculadora de decimal pra binario.py
@@ -0,0 +1,8 @@
+
+num=(int(input('Digite um número:')))
+esc=(str(input('Você quer que transforme em binário,octal ou hexadecimal?')))
+if esc=='binário':
+    while num!=1 or 0:
+     num2= num % 2
+    print(num2) 
+
---
+++ Codigos/comparador numero maior.py
@@ -0,0 +1,9 @@
+num1=float(input('Digite um numero:'))
+num2=float(input('DIgite outro numero:'))
+if num1>num2:
+    print('O primeiro valor é maior')
+elif num2>num1:
+    print('O segundo valor e maior')
+else:
+    print('não existe valor maior, os dois são iguais')
+    
